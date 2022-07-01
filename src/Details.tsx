614b57bd349a6145571032c19e162b098a75b8a6
vikingviolinist <misopetrik@gmail.com>
2022-07-01 23:10:43 +0200

    Refactor Details to TS

--- src/Details.tsx
+++ src/Details.tsx
@@ -3,21 +3,32 @@ import { useParams } from "react-router-dom";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
 import ThemeContext from "./ThemeContext";
+import { Animal, PetAPIResponse, Pet } from "./APIResponsesTypes";
 
 const Modal = lazy(() => import("./Modal"));
 
-class Details extends Component {
-  state = { loading: true, showModal: false };
+class Details extends Component<{ params: { id?: string } }> {
+  state = {
+    loading: true,
+    showModal: false,
+    animal: "" as Animal,
+    breed: "",
+    city: "",
+    state: "",
+    description: "",
+    name: "",
+    images: [] as string[],
+  };
 
   async componentDidMount() {
     const res = await fetch(
       `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
     );
-    const json = await res.json();
+    const json = (await res.json()) as PetAPIResponse;
     this.setState(Object.assign({ loading: false }, json.pets[0]));
   }
   toggleModal = () => this.setState({ showModal: !this.state.showModal });
-  adopt = () => (window.location = "http://bit.ly/pet-adopt");
+  adopt = () => (window.location.href = "http://bit.ly/pet-adopt");
   render() {
     if (this.state.loading) {
       return <h2>loading … </h2>;
@@ -61,7 +72,7 @@ class Details extends Component {
 }
 
 const WrappedDetails = () => {
-  const params = useParams();
+  const params = useParams<{ id: string }>();
   return (
     <ErrorBoundary>
       <Details params={params} />
