5d27d18f12cc9968218fc4f4764d4e3f07030173
vikingviolinist <misopetrik@gmail.com>
2022-07-13 13:23:30 +0200

    🚨 Use dynamic render method

--- src/ClientApp.js
+++ src/ClientApp.js
@@ -1,8 +1,10 @@
-import { hydrate } from "react-dom";
+import { hydrate, render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-hydrate(
+const renderMethod = module.hot ? render : hydrate;
+
+renderMethod(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
